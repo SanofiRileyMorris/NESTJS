465eedacd6131c4c7ee357a9c2c53a19e24eba8b
SanofiRileyMorris <115551504+SanofiRileyMorris@users.noreply.github.com>
2024-08-30 10:24:31 -0400

    Prisma services

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -2,7 +2,7 @@ import { Injectable, HttpException, HttpStatus, Logger } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { lastValueFrom } from 'rxjs';
 import { Movie, Movies } from './movie.interface';
-import { AxiosRequestConfig } from 'axios';
+import { PrismaService } from 'src/middleware/prisma.service';
 
 @Injectable()
 export class MoviesService {
@@ -10,7 +10,7 @@ export class MoviesService {
     private readonly apiKey = process.env.APP_ACCESS_TOKEN;
     private readonly logger = new Logger(MoviesService.name);
 
-    constructor(private readonly httpService: HttpService) { }
+    constructor(private readonly httpService: HttpService, private prisma: PrismaService) { }
 
     async getMovie(movieId: string): Promise<Movie> {
 
@@ -72,6 +72,66 @@ export class MoviesService {
         } catch (error) {
             throw new HttpException('Failed to list movies', HttpStatus.BAD_REQUEST);
         }
+    }
+
+    // get favourite movies from Prisma
+    //
+    async listFavMovies(userId: number) {
+        const favMovies = await this.prisma.movie.findMany({
+            where: {
+                userMovies: {
+                    every: {
+                        user_id: userId
+                    }
+                }
+            }
+        })
+
+        return { results: favMovies }
+    }
+
+    async saveFavMovie(userId: number, input: { movie: string, movieId: number }) {
+        const movie = await this.prisma.movie.findFirst({
+            where: {
+                movie_id: input.movieId
+            },
+            include: {
+                userMovies: true
+            }
+        })
+
+        if (movie) {
+            const userMovie = movie.userMovies.find((element) => element.user_id === userId)
+            await this.prisma.userMovie.upsert({
+                where: {
+                    id: userMovie.id,
+                },
+                create: {
+                    createdAt: new Date(),
+                    user_id: userId,
+                    movie_id: movie.id
+                },
+                update: {
+                    deletedAt: null,
+                }
+            })
+            return { status: "Ok" }
+
+        }
+
+        this.prisma.movie.create({
+            data: {
+                movie_id: input.movieId,
+                name: input.movie,
+                userMovies: {
+                    // @ts-expect-error
+                    create: {
+                        user_id: userId,
+                    }
+                }
+            }
+        })
+        return { status: "Ok" }
     }
 }
 
